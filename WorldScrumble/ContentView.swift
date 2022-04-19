19a28f18e9da66fc101ec858e563dc60941d15b7
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-04-19 16:17:24 -0300

    Initial Commit

---
+++ WorldScrumble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorldScrumble
+//
+//  Created by Rodrigo Tarouco on 19/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WorldScrumble/WorldScrumbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldScrumbleApp.swift
+//  WorldScrumble
+//
+//  Created by Rodrigo Tarouco on 19/04/22.
+//
+
+import SwiftUI
+
+@main
+struct WorldScrumbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
